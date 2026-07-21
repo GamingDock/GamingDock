ad667e6c47a64e6fb05dcfd2c51ae1c07b298adb
GamingDock <millionairethoughts13@gmail.com>
2026-07-21 08:45:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,28 +3,49 @@ document.addEventListener("DOMContentLoaded", function () {
 const searchBtn = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
 
-if (searchBtn) {
-
 searchBtn.addEventListener("click", function () {
 
 const searchValue = searchInput.value.trim().toLowerCase();
 
-if (searchValue === "roblox") {
+const games = document.querySelectorAll(".game-card");
 
-window.location.href = "game.html";
+let found = false;
 
-} else if (searchValue === "") {
+games.forEach(function(game){
+
+const gameName = game.getAttribute("data-game");
+
+if (
+searchValue === "" ||
+gameName.includes(searchValue)
+){
+
+game.style.display = "block";
+
+if(searchValue !== ""){
+found = true;
+}
+
+}else{
+
+game.style.display = "none";
+
+}
+
+});
+
+if(searchValue === ""){
 
 alert("Please enter a game name.");
 
-} else {
+}
+
+else if(!found){
 
 alert("Game not found.");
 
 }
 
 });
-
-}
 
 });
